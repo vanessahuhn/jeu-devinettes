1f489d034d0fc8c2eba0a6a3072819ac08123b22
vanessahuhn <vanessa.huhn@gmail.com>
2018-07-26 15:28:54 +0200

    décomposition du problème

--- src/jeuDevinette/Game.java
+++ src/jeuDevinette/Game.java
@@ -1,5 +1,20 @@
 package jeuDevinette;
 
-public class Game {
-
+public class Game 
+{
+	
+	//remainigAttempts 10
+	
+	//proposer un secretNumber au hasard
+	
+	//tant que essais > 0 && nbFound(false)
+	//demander à l'user un numéro entre 1 et 1000
+	
+	//si userInput > secretNumber
+		//afficher Your guess is greater than the secret number.
+	//si userInput < secretNumber
+		//afficher Your guess is smaller than the secret number.
+	//si userInput = secretNumber
+		//afficher Your guess is correct. Congratulations!
+		//nbFound = true;
 }
